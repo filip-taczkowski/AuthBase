622fb55e00eaeba15bc82ec8ec991dd2d00968a8
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-04-16 20:21:44 +0200

    Add endpoints for user profile and settings

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+	res.render('logged');
 });
 
 router.get('/no-permission', (req, res) => {
-  res.render('noPermission');
+	res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+	if (req.user) {
+		res.render('userProfile');
+	} else {
+		res.render('noPermission');
+	}
+});
+
+router.get('/profile/settings', (req, res) => {
+	if (req.user) {
+		res.render('userSettings');
+	} else {
+		res.render('noPermission');
+	}
+});
 
 module.exports = router;

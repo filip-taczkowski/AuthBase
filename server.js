649bea8aba02458dc69e3286761b1720f7c6846b
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-04-16 20:02:49 +0200

    Refactor code

--- server.js
+++ server.js
@@ -3,37 +3,23 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-// configure passport provider options
-passport.use(new GoogleStrategy({
-  clientID: '9754491gfdgfdg5fdalmg9111fe3ahrbu.apps.googleusercontent.com',
-  clientSecret: 'OfHsdfsdfgMZTLtlIMoe0',
-  callbackURL: 'http://localhost:8000/auth/callback'
-}, (accessToken, refreshToken, profile, done) => {
-done(null, profile);
-}));
+// init session mechanism
+app.use(session({ secret: 'anything' }));
 
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
 
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
-
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,13 +29,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
